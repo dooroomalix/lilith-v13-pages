c64c48eaa9d8081bfd9a44ec59436572f76e109c
dooroomalix <dooroomalix@gmail.com>
2026-06-29 17:38:00 +0900

    Clear old factory cache

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,10 @@
-const CACHE = "lilith-v13-flat-pwa-v5-3";
-const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icon-192.svg",
-  "./icon-512.svg"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
+  self.clients.claim();
 });
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(
-    caches.match(event.request)
-      .then(hit => hit || fetch(event.request))
-      .catch(() => caches.match("./index.html"))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
 });
